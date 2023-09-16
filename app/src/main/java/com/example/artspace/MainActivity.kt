e3921f08303e81293d2ab965487297280591f2db
Christella De Sousa <desousa.christella@proton.me>
2023-09-16 16:12:03 +0200

    Rename composables and create functions for previous and next img clicks

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArtSpaceLogic()
+                    ArtSpaceApp()
                 }
             }
         }
@@ -58,142 +58,146 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ArtSpaceLogic(modifier: Modifier = Modifier) {
+fun ArtSpaceApp(modifier: Modifier = Modifier) {
     var currentImg by remember {
         mutableStateOf(1)
     }
 
-    when (currentImg) {
+    fun clickPrevious() {
+        if (currentImg == 1) {
+            currentImg = 11
+        } else {
+            currentImg--
+        }
+    }
+    fun clickNext() {
+        if (currentImg == 11) {
+            currentImg = 1
+        } else {
+            currentImg++
+        }
+    }
+
+        when (currentImg) {
         1 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.the_fall_of_phaeton_1990_1_1),
                 imgDesc = stringResource(R.string.painting_phaeton_rubens),
                 title = R.string.painting_phaeton_rubens,
                 artist = R.string.artist_rubens,
                 date = R.string.date_phaeton_rubens,
-                previousImg = {
-                    if(currentImg == 1) {
-                        currentImg = 11
-                    } else {
-                        currentImg--
-                    } },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         2 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.madame_d_aguesseau_de_fresnes_1946_7_16),
                 imgDesc = stringResource(R.string.painting_fresnes_le_brun),
                 title = R.string.painting_fresnes_le_brun,
                 artist = R.string.artist_le_brun,
                 date = R.string.date_fresnes_le_brun,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         3 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.the_fence_1985_64_31),
                 imgDesc = stringResource(R.string.painting_fence_pissarro),
                 title = R.string.painting_fence_pissarro,
                 artist = R.string.artist_pissarro,
                 date = R.string.date_fence_pissarro,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         4 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.a_creek_in_st__thomas__virgin_islands__1985_64_29),
                 imgDesc = stringResource(R.string.painting_creek_pissarro),
                 title = R.string.painting_creek_pissarro,
                 artist = R.string.artist_pissarro,
                 date = R.string.date_creek_pissarro,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         5 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.flower_beds_in_holland_1983_1_21),
                 imgDesc = stringResource(R.string.painting_flower_beds_van_gogh),
                 title = R.string.painting_flower_beds_van_gogh,
                 artist = R.string.artist_van_gogh,
                 date = R.string.date_flower_beds_van_gogh,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         6 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.roses_1991_67_1),
                 imgDesc = stringResource(R.string.painting_roses_van_gogh),
                 title = R.string.painting_roses_van_gogh,
                 artist = R.string.artist_van_gogh,
                 date = R.string.date_roses_van_gogh,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         7 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.ginevra_de__benci__obverse__1967_6_1_a),
                 imgDesc = stringResource(R.string.painting_da_vinci),
                 title = R.string.painting_da_vinci,
                 artist = R.string.artist_da_vinci,
                 date = R.string.date_da_vinci,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         8 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.the_artist_s_garden_at_vetheuil_1970_17_45),
                 imgDesc = stringResource(R.string.painting_garden_monet),
                 title = R.string.painting_garden_monet,
                 artist = R.string.artist_monet,
                 date = R.string.date_garden_monet,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         9 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.still_life_with_milk_jug_and_fruit_1972_9_5),
                 imgDesc = stringResource(R.string.painting_still_life_cezanne),
                 title = R.string.painting_still_life_cezanne,
                 artist = R.string.artist_cezanne,
                 date = R.string.date_still_life_cezanne,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         10 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.the_mill_1942_9_62),
                 imgDesc = stringResource(R.string.painting_mill_rembrandt),
                 title = R.string.painting_mill_rembrandt,
                 artist = R.string.artist_rembrandt,
                 date = R.string.date_mill_rembrandt,
-                previousImg = { currentImg-- },
-                nextImg = { currentImg++ }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
         11 -> {
-            ArtSpaceLayout(
+            ArtworkLayout(
                 img = painterResource(R.drawable.faust_1949_1_40),
                 imgDesc = stringResource(R.string.painting_faust_rembrandt),
                 title = R.string.painting_faust_rembrandt,
                 artist = R.string.artist_rembrandt,
                 date = R.string.date_faust_rembrandt,
-                previousImg = { currentImg-- },
-                nextImg = {
-                    if (currentImg == 11) {
-                        currentImg = 1
-                    } else {
-                        currentImg++
-                    }
-                }
+                previousImg = { clickPrevious() },
+                nextImg = { clickNext() }
             )
         }
     }
@@ -201,7 +205,7 @@ fun ArtSpaceLogic(modifier: Modifier = Modifier) {
 
 
 @Composable
-fun ArtSpaceLayout(
+fun ArtworkLayout(
     img: Painter,
     imgDesc: String,
     @StringRes title: Int,
@@ -276,6 +280,6 @@ fun ChangeImg(btnName: String, changeImg: () -> Unit, modifier: Modifier = Modif
 @Composable
 fun ArtSpaceLayoutPreview() {
     ArtSpaceTheme {
-        ArtSpaceLogic()
+        ArtSpaceApp()
     }
 }
